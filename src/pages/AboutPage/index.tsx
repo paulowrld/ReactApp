21997185b552c5556efa410a8c7ae32dfe9e679d
Paulo Pellizzari <pauloric6@hotmail.com>
2024-06-25 17:17:28 -0300

    ajustes

--- src/pages/AboutPage/index.tsx
+++ src/pages/AboutPage/index.tsx
@@ -1,16 +1,53 @@
 import { DefaultPageType } from "../Types";
-import { Button, View, Text } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 
 const AboutPage: DefaultPageType = ({ navigation }) => {
     return(
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Tela Sobre</Text>
-            <Button
-                title="Back to Home"
+        <View style={styles.container}>
+            <Text style={styles.header}>Sobre o App</Text>
+            <Text style={styles.content}>
+                Bem-vindo ao nosso aplicativo, uma plataforma inovadora dedicada a promover o Trabalho Decente e o Crescimento Econômico, alinhada com os objetivos da ODS 8. Nosso aplicativo serve como uma ponte entre empresas e profissionais, facilitando o cadastro e a aplicação para vagas de emprego. Embora esta versão seja uma demonstração, ela é um passo importante em direção a um mercado de trabalho mais justo e acessível.
+            </Text>
+            <TouchableOpacity
                 onPress={() => navigation.navigate("Home")}
-            />
+                style={styles.button}>
+                <Text style={styles.buttonText}>Voltar para Início</Text>
+            </TouchableOpacity>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 20,
+        backgroundColor: '#fff',
+    },
+    header: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginBottom: 20,
+        color: '#333',
+    },
+    content: {
+        fontSize: 16,
+        lineHeight: 24,
+        textAlign: 'justify',
+        marginBottom: 20,
+        color: '#666',
+    },
+    button: {
+        paddingHorizontal: 20,
+        paddingVertical: 10,
+        borderRadius: 5,
+        backgroundColor: '#0066cc',
+    },
+    buttonText: {
+        color: '#fff',
+        textAlign: 'center',
+    }
+});
 
 export default AboutPage;
--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,28 +1,51 @@
 import useAuthentication from "@/service/AuthenticationService/modules";
 import { DefaultPageType } from "../Types";
-import { Button, View, Text } from 'react-native';
+import { Button, View, Text, StyleSheet } from 'react-native';
 
 const HomePage: DefaultPageType = ({ navigation }) => {
 
     const { logout } = useAuthentication();
 
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Tela inicial</Text>
-            <Button
-                title="Ver Sobre"
-                onPress={() => navigation.navigate('About')}
-            />
-            <Button
-                title="Cadastrar Vagas"
-                onPress={() => navigation.navigate('CadastrarVagas')}
-            />
-             <Button
-                title="Aplicar Vagas"
-                onPress={() => navigation.navigate('AplicarVagas')}
-            />
+        <View style={styles.container}>
+            <View style={styles.buttonContainer}>
+                <Button
+                    title="Ver Sobre"
+                    onPress={() => navigation.navigate('About')}
+                />
+            </View>
+            <View style={styles.buttonContainer}>
+                <Button
+                    title="Cadastrar Vagas"
+                    onPress={() => navigation.navigate('CadastrarVagas')}
+                />
+            </View>
+            <View style={styles.buttonContainer}>
+                <Button
+                    title="Aplicar Vagas"
+                    onPress={() => navigation.navigate('AplicarVagas')}
+                />
+            </View>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#f7f7f7', // Um fundo claro
+    },
+    title: {
+        fontSize: 22,
+        marginBottom: 20,
+        fontWeight: 'bold',
+    },
+    buttonContainer: {
+        marginBottom: 10,
+        width: '80%', // Largura dos botões
+    }
+});
 
 export default HomePage;
